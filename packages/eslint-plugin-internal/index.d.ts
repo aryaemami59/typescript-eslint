2358349ef5f4589cd69da134eecee611224e9268
Arya Emami <aryaemami59@yahoo.com>
2024-09-05 20:27:55 -0500

    Fix `@typescript-eslint/eslint-plugin-internal`

--- packages/eslint-plugin-internal/index.d.ts
+++ packages/eslint-plugin-internal/index.d.ts
@@ -1,6 +1,6 @@
 import type { FlatConfig } from '@typescript-eslint/utils/ts-eslint';
 
-import type rules from './rules';
+import type rules from './src/rules/index';
 
 declare const cjsExport: {
   meta: FlatConfig.PluginMeta;
