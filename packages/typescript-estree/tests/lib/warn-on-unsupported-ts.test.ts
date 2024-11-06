06c94823c6ed46af0667b4b136f3a29bf39a024d
Arya Emami <aryaemami59@yahoo.com>
2024-11-06 04:01:42 -0600

    Fix tests for `@typescript-eslint/typescript-estree`

--- packages/typescript-estree/tests/lib/warn-on-unsupported-ts.test.ts
+++ packages/typescript-estree/tests/lib/warn-on-unsupported-ts.test.ts
@@ -5,8 +5,6 @@ import type * as Parser from '../../src/parser';
 
 vi.mock('semver');
 
-const resetIsTTY = process.stdout.isTTY;
-
 describe('Warn on unsupported TypeScript version', () => {
   let parser: typeof Parser;
 
@@ -16,13 +14,13 @@ describe('Warn on unsupported TypeScript version', () => {
   afterEach(() => {
     vi.resetModules();
     vi.resetAllMocks();
-    process.stdout.isTTY = resetIsTTY;
+    vi.unstubAllGlobals();
   });
 
   it('should warn the user if they are using an unsupported TypeScript version', () => {
     vi.mocked(semver.satisfies).mockReturnValue(false);
     vi.spyOn(console, 'log').mockImplementation(() => {});
-    process.stdout.isTTY = true;
+    vi.stubGlobal('process', { ...process, stdout: { isTTY: true } });
 
     parser.parse('');
     expect(console.log).toHaveBeenCalledWith(
@@ -30,25 +28,31 @@ describe('Warn on unsupported TypeScript version', () => {
         'WARNING: You are currently running a version of TypeScript which is not officially supported by @typescript-eslint/typescript-estree.',
       ),
     );
+
+    vi.unstubAllGlobals();
   });
 
   it('should warn the user if they are running on a non TTY process and a custom loggerFn was passed', () => {
     vi.mocked(semver.satisfies).mockReturnValue(false);
     const loggerFn = vi.fn();
-    process.stdout.isTTY = false;
+    vi.stubGlobal('process', { ...process, stdout: { isTTY: false } });
 
     parser.parse('', {
       loggerFn,
     });
     expect(loggerFn).toHaveBeenCalled();
+
+    vi.unstubAllGlobals();
   });
 
   it('should not warn the user if they are running on a non TTY process and a custom loggerFn was not passed', () => {
     vi.mocked(semver.satisfies).mockReturnValue(false);
     vi.spyOn(console, 'log').mockImplementation(() => {});
-    process.stdout.isTTY = false;
+    vi.stubGlobal('process', { ...process, stdout: { isTTY: false } });
 
     parser.parse('');
     expect(console.log).not.toHaveBeenCalled();
+
+    vi.unstubAllGlobals();
   });
 });
