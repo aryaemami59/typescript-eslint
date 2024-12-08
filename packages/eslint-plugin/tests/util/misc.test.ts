358909066361d42896a259ba65d64343b530399f
Arya Emami <aryaemami59@yahoo.com>
2024-12-08 12:19:08 -0600

    Remove explicit imports from `vitest`

--- packages/eslint-plugin/tests/util/misc.test.ts
+++ packages/eslint-plugin/tests/util/misc.test.ts
@@ -1,5 +1,3 @@
-import { describe, expect, it } from 'vitest';
-
 import * as misc from '../../src/util/misc';
 
 describe('formatWordList', () => {
--- packages/scope-manager/tests/eslint-scope/es6-default-parameters.test.ts
+++ packages/scope-manager/tests/eslint-scope/es6-default-parameters.test.ts
@@ -1,5 +1,3 @@
-import { describe, expect, it } from 'vitest';
-
 import { getRealVariables, parseAndAnalyze } from '../test-utils';
 
 function forEach<T extends string>(
