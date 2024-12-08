55bb9249022ae318d980cf3004c1bd294d545e3f
Arya Emami <aryaemami59@yahoo.com>
2024-12-08 13:55:46 -0600

    Temporarily revert the changes in `@typescript-eslint/typescript-estree`

--- packages/typescript-estree/tests/lib/createProjectService.test.ts
+++ packages/typescript-estree/tests/lib/createProjectService.test.ts
@@ -1,5 +1,6 @@
 import debug from 'debug';
 import * as ts from 'typescript';
+import * as tsserver from 'typescript/lib/tsserverlibrary.js';
 import {
   afterAll,
   afterEach,
@@ -10,12 +11,24 @@ import {
   vi,
 } from 'vitest';
 
-import { createProjectService } from '../../src/create-program/createProjectService.js';
-import { getParsedConfigFile } from '../../src/create-program/getParsedConfigFile.js';
+import type { ProjectServiceSettings } from '../../src/create-program/createProjectService.js';
+import type { ProjectServiceOptions } from '../../src/parser-options.js';
 
-vi.mock(import('../../src/create-program/getParsedConfigFile.js'), () => ({
-  getParsedConfigFile: vi.fn(),
-}));
+import { getParsedConfigFile } from '../../src/create-program/getParsedConfigFile.js';
+import { validateDefaultProjectForFilesGlob } from '../../src/create-program/validateDefaultProjectForFilesGlob.js';
+
+vi.mock(
+  import('../../src/create-program/getParsedConfigFile.js'),
+  async importOriginal => {
+    const actual = await importOriginal();
+    const getParsedConfigFile = vi.fn(actual.getParsedConfigFile);
+
+    return {
+      ...actual,
+      getParsedConfigFile,
+    };
+  },
+);
 
 vi.mock(import('typescript/lib/tsserverlibrary.js'), async importOriginal => {
   const actual = await importOriginal();
@@ -47,6 +60,145 @@ vi.mock(import('typescript/lib/tsserverlibrary.js'), async importOriginal => {
     },
   };
 });
+
+const DEFAULT_PROJECT_MATCHED_FILES_THRESHOLD = 8;
+
+const log = debug('typescript-eslint:typescript-estree:createProjectService');
+const logTsserverErr = debug(
+  'typescript-eslint:typescript-estree:tsserver:err',
+);
+const logTsserverInfo = debug(
+  'typescript-eslint:typescript-estree:tsserver:info',
+);
+const logTsserverPerf = debug(
+  'typescript-eslint:typescript-estree:tsserver:perf',
+);
+const logTsserverEvent = debug(
+  'typescript-eslint:typescript-estree:tsserver:event',
+);
+
+const doNothing = (): void => {};
+
+const createStubFileWatcher = (): ts.FileWatcher => ({
+  close: doNothing,
+});
+
+function createProjectService(
+  optionsRaw: boolean | ProjectServiceOptions | undefined,
+  jsDocParsingMode: ts.JSDocParsingMode | undefined,
+  tsconfigRootDir: string | undefined,
+): ProjectServiceSettings {
+  const optionsRawObject = typeof optionsRaw === 'object' ? optionsRaw : {};
+  const options = {
+    defaultProject: 'tsconfig.json',
+    ...optionsRawObject,
+  };
+  validateDefaultProjectForFilesGlob(options.allowDefaultProject);
+
+  const system: ts.server.ServerHost = {
+    ...tsserver.sys,
+    clearImmediate,
+    clearTimeout,
+    setImmediate,
+    setTimeout,
+    watchDirectory: createStubFileWatcher,
+    watchFile: createStubFileWatcher,
+    ...(!options.loadTypeScriptPlugins && {
+      require: () => ({
+        error: {
+          message:
+            'TypeScript plugins are not required when using parserOptions.projectService.',
+        },
+        module: undefined,
+      }),
+    }),
+  };
+
+  const logger: ts.server.Logger = {
+    close: doNothing,
+    endGroup: doNothing,
+    getLogFileName: (): undefined => undefined,
+    hasLevel: (): boolean => true,
+    info(s) {
+      this.msg(s, tsserver.server.Msg.Info);
+    },
+    loggingEnabled: (): boolean =>
+      logTsserverInfo.enabled ||
+      logTsserverErr.enabled ||
+      logTsserverPerf.enabled,
+    msg: (s, type) => {
+      switch (type) {
+        case tsserver.server.Msg.Err:
+          logTsserverErr(s);
+          break;
+        case tsserver.server.Msg.Perf:
+          logTsserverPerf(s);
+          break;
+        default:
+          logTsserverInfo(s);
+      }
+    },
+    perftrc(s) {
+      this.msg(s, tsserver.server.Msg.Perf);
+    },
+    startGroup: doNothing,
+  };
+
+  log('Creating project service with: %o', options);
+
+  const service = new tsserver.server.ProjectService({
+    cancellationToken: { isCancellationRequested: (): boolean => false },
+    eventHandler: logTsserverEvent.enabled
+      ? (e): void => {
+          logTsserverEvent(e);
+        }
+      : undefined,
+    host: system,
+    jsDocParsingMode,
+    logger,
+    session: undefined,
+    useInferredProjectPerProjectRoot: false,
+    useSingleInferredProject: false,
+  });
+
+  service.setHostConfiguration({
+    preferences: {
+      includePackageJsonAutoImports: 'off',
+    },
+  });
+
+  log('Enabling default project: %s', options.defaultProject);
+  let configFile: ts.ParsedCommandLine | undefined;
+
+  try {
+    configFile = getParsedConfigFile(
+      tsserver,
+      options.defaultProject,
+      tsconfigRootDir,
+    );
+  } catch (error) {
+    if (optionsRawObject.defaultProject) {
+      throw new Error(
+        `Could not read project service default project '${options.defaultProject}': ${(error as Error).message}`,
+      );
+    }
+  }
+
+  if (configFile) {
+    service.setCompilerOptionsForInferredProjects(
+      configFile.options as ts.server.protocol.InferredProjectCompilerOptions,
+    );
+  }
+
+  return {
+    allowDefaultProject: options.allowDefaultProject,
+    lastReloadTimestamp: performance.now(),
+    maximumDefaultProjectFileMatchCount:
+      options.maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING ??
+      DEFAULT_PROJECT_MATCHED_FILES_THRESHOLD,
+    service,
+  };
+}
 
 const mockGetParsedConfigFile = vi.mocked(getParsedConfigFile);
 
